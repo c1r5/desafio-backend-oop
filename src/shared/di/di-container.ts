1aa62a220e38df193158ecf557fec62a952a304e
c1r5 <contato.ericjoseph@gmail.com>
2025-02-28 22:46:22 -0300

    fix: muda o symbol para o uso do TransactionController concreto

--- src/shared/di/di-container.ts
+++ src/shared/di/di-container.ts
@@ -1,17 +1,25 @@
 import { Container } from "inversify";
 import TransactionRepository from "modules/transaction/infra/repositories/transaction-repository";
 import TransactionController from "modules/transaction/interfaces/controllers/transaction-controller";
+import UserRepository from "modules/users/infra/repositories/user-repository";
+import UserController from "modules/users/interfaces/controllers/user-controller";
 import ControllerModel from "shared/domain/models/controller-model";
 
 
 const TYPES = {
+  UserRepository: Symbol.for('UserRepository'),
+  UserController: Symbol.for('UserController'),
   TransactionRepository: Symbol.for('TransactionRepository'),
-  TransactionController: Symbol.for('ControllerModel')
+  TransactionController: Symbol.for('TransactionController')
 };
 
 const container = new Container()
 
+
+container.bind<UserRepository>(TYPES.UserRepository).to(UserRepository).inSingletonScope()
+container.bind<UserController>(TYPES.UserController).to(UserController)
+
 container.bind<TransactionRepository>(TYPES.TransactionRepository).to(TransactionRepository).inSingletonScope()
-container.bind<ControllerModel>(TYPES.TransactionController).to(TransactionController)
+container.bind<TransactionController>(TYPES.TransactionController).to(TransactionController)
 
 export { container, TYPES }
