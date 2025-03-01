5b9b5375ba195bb7f195089bcb21dd67ca892280
c1r5 <contato.ericjoseph@gmail.com>
2025-02-28 23:16:45 -0300

    feat: registra o controller de usuarios na aplicação principal

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import TransactionController from "modules/transaction/interfaces/controllers/tr
 import "reflect-metadata";
 import App from "app/app";
 import { container, TYPES } from "shared/di/di-container";
+import UserController from "modules/users/interfaces/controllers/user-controller";
 
 const app = new App()
 
 const transaction_controller = container.get<TransactionController>(TYPES.TransactionController)
+const user_controller = container.get<UserController>(TYPES.UserController)
 
 app
   .register_api_controller(transaction_controller)
+  .register_api_controller(user_controller)
