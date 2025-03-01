646971364b59433ea6c6a560deeddf835d7b6a9a
c1r5 <contato.ericjoseph@gmail.com>
2025-02-28 23:14:09 -0300

    feat: adiciona os usecases no conteiner de dependency injection

--- src/shared/di/di-container.ts
+++ src/shared/di/di-container.ts
@@ -1,25 +1,30 @@
 import { Container } from "inversify";
-import TransactionRepository from "modules/transaction/infra/repositories/transaction-repository";
+import TransactionUseCases from "modules/transaction/domain/usecases/transaction-usecases";
+import TransactionRepositoryImpl from "modules/transaction/infra/repositories/transaction-repository-impl";
 import TransactionController from "modules/transaction/interfaces/controllers/transaction-controller";
-import UserRepository from "modules/users/infra/repositories/user-repository";
+import UserUseCases from "modules/users/domain/usecases/user-usecases";
+import UserRepositoryImpl from "modules/users/infra/repositories/user-repository-impl";
 import UserController from "modules/users/interfaces/controllers/user-controller";
-import ControllerModel from "shared/domain/models/controller-model";
 
 
 const TYPES = {
   UserRepository: Symbol.for('UserRepository'),
+  UserUseCases: Symbol.for('UserUseCases'),
   UserController: Symbol.for('UserController'),
   TransactionRepository: Symbol.for('TransactionRepository'),
+  TransactionUseCases: Symbol.for('TransactionUseCases'),
   TransactionController: Symbol.for('TransactionController')
 };
 
 const container = new Container()
 
 
-container.bind<UserRepository>(TYPES.UserRepository).to(UserRepository).inSingletonScope()
+container.bind<UserRepositoryImpl>(TYPES.UserRepository).to(UserRepositoryImpl).inSingletonScope()
+container.bind<UserUseCases>(TYPES.UserUseCases).to(UserUseCases)
 container.bind<UserController>(TYPES.UserController).to(UserController)
 
-container.bind<TransactionRepository>(TYPES.TransactionRepository).to(TransactionRepository).inSingletonScope()
+container.bind<TransactionRepositoryImpl>(TYPES.TransactionRepository).to(TransactionRepositoryImpl).inSingletonScope()
+container.bind<TransactionUseCases>(TYPES.TransactionUseCases).to(TransactionUseCases)
 container.bind<TransactionController>(TYPES.TransactionController).to(TransactionController)
 
 export { container, TYPES }
