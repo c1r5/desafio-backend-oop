42b390a1a72283209f78786159cb8934e67d5b87
c1r5 <contato.ericjoseph@gmail.com>
2025-03-01 23:09:02 -0300

    fix: gera automaticamente o userId e o timestamp

--- src/modules/users/domain/entities/user-entity.ts
+++ src/modules/users/domain/entities/user-entity.ts
@@ -1,20 +1,20 @@
 import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
 
 export type User = {
-  userId: string
+  userId?: string
   name: string
   email: string
   phone: string
   password: string
   document: string
-  createdAt: Date
-  updatedAt: Date
-  lastLogin: Date
+  createdAt?: Date
+  updatedAt?: Date
+  lastLogin?: Date
 }
 
 @Entity({ name: "users" })
 export default class UserEntity extends BaseEntity {
-  @PrimaryColumn({ type: 'uuid'})
+  @PrimaryColumn({ type: 'uuid', generated: 'uuid'})
   userId!: string;
 
   @Column({ type: 'varchar', length: 100})
@@ -32,26 +32,22 @@ export default class UserEntity extends BaseEntity {
   @Column({ type: 'varchar', length: 100})
   password!: string;
 
-  @Column({ type: 'numeric' })
+  @Column({ type: 'bigint', default: 0})
+  balance!: BigInt
+
+  @Column({ type: 'numeric', default: () => 'CURRENT_TIMESTAMP' })
   createdAt!: number;
 
-  @Column({ type: 'numeric' })
+  @Column({ type: 'numeric', default: () => 'CURRENT_TIMESTAMP' })
   updatedAt!: number;
 
-  @Column({ type: 'numeric' })
-  lastLogin!: number;
-
   static from(user: User) : UserEntity {
-    let user_entity = new UserEntity()
+    let user_entity = this.create()
     user_entity.name = user.name
     user_entity.email = user.email
     user_entity.phone = user.phone
     user_entity.document = user.document
     user_entity.password = user.password
-    user_entity.password = user.password
-    user_entity.createdAt = user.createdAt.getTime()
-    user_entity.updatedAt = user.updatedAt.getTime()
-    user_entity.lastLogin = user.lastLogin.getTime()
     return user_entity
   }
 }
