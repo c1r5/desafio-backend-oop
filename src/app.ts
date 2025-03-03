c4a85db957d69c059db98923effe08e11ffdbd6f
c1r5 <contato.ericjoseph@gmail.com>
2025-03-03 14:31:44 -0300

    fix: refatora a inicialização do app para um async context e refatora o register_api_controllers para registrar as rotas para cada controller

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import fastify, {
   FastifyInstance,
-  RawServerBase,
-  RouteOptions
+  RawServerDefault,
 } from "fastify";
 
-import swagger from '@fastify/swagger';
-import swaggerUi from '@fastify/swagger-ui';
+
 import EventEmitter from "node:events";
 import ControllerModel from "@/shared/domain/models/controller-model";
 
@@ -14,37 +12,36 @@ export default class App extends EventEmitter<{
   error: [Error],
   connect: [string]
 }> {
-  public mock_server: RawServerBase
-
-  private app: FastifyInstance
+  private readonly app: FastifyInstance
   private port: number
-  private routes: RouteOptions[] = [];
 
   constructor(port?: number) {
     super()
 
     this.port = port || 3000
     this.app = fastify()
-    this.mock_server = this.app.server
+  }
 
-    this.setup_swagger()
+  async mock_on_ready(): Promise<RawServerDefault> {
+    await this.app.ready()
+    return this.app.server
   }
 
   register_api_controller(controller: ControllerModel): this {
-    this.routes.push(...controller.options)
 
-    return this
-  }
-
-  start() {
     this.app.register(server => {
-      for (const route of this.routes) {
+      for (const route of controller.options) {
         console.log(`[+] Routing ${route.url}`)
         server.route(route)
       }
     }, { prefix: '/api' });
 
-    this.app.listen({ port: this.port })
+    return this
+  }
+
+  async start() {
+
+    await this.app.listen({ port: this.port })
       .then(server => {
         console.log(`[+] Server listening at: ${server}`)
         this.emit('connect', server)
@@ -52,28 +49,5 @@ export default class App extends EventEmitter<{
       .catch(err => {
         this.emit('error', err)
       })
-  }
-
-  private setup_swagger = () => {
-    this.app.register(swaggerUi, {
-      routePrefix: '/docs',
-      uiConfig: {
-        docExpansion: 'full',
-        deepLinking: false,
-      },
-    });
-
-    this.app.register(swagger, {
-      openapi: {
-        info: {
-          title: 'Fastify TypeScript API',
-          description: 'A sample API with Fastify, TypeScript, and Swagger',
-          version: '1.0.0',
-        },
-        servers: [
-          { url: 'http://localhost:3000' },
-        ],
-      },
-    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,14 @@ import { container } from "@/shared/infra/di/di-container";
 import {TYPES} from "@/shared/infra/di/di-types";
 import {UserController} from "@/modules/users/interfaces/controllers/user-controller";
 
-const app = new App()
+(async () => {
+    const app = new App()
 
-const transaction_controller = container.get<TransactionController>(TYPES.TransactionController)
-const user_controller = container.get<UserController>(TYPES.UserController)
+    const transaction_controller = container.get<TransactionController>(TYPES.TransactionController)
+    const user_controller = container.get<UserController>(TYPES.UserController)
 
-app
+    await app
     .register_api_controller(transaction_controller)
     .register_api_controller(user_controller)
     .start()
-
-export {
-    app
-}
+})();
