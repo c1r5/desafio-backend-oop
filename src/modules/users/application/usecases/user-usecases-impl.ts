c2956c6c67f52bd968de394999bcd1585ba186ce
c1r5 <contato.ericjoseph@gmail.com>
2025-03-27 00:28:54 -0300

    feat: implements user.updated event in update_user() method from userr-usecasees

--- src/modules/users/application/usecases/user-usecases-impl.ts
+++ src/modules/users/application/usecases/user-usecases-impl.ts
@@ -6,7 +6,7 @@ import UserEntity from "@/modules/users/domain/entities/user-entity";
 import {EventBusInterface} from "@/shared/domain/models/event-models/event-bus-interface";
 import {CannotCreateUser, UserAlreadyExist} from "@/modules/users/application/errors/create-errors";
 import {QueryFailedError} from "typeorm";
-import {UserCreatedPayload} from "@/shared/domain/models/event-models/user-events";
+import {UserCreatedPayload, UserUpdatedPayload} from "@/shared/domain/models/event-models/user-events";
 
 @injectable()
 export default class UserUsecasesImpl implements UserUseCases {
@@ -26,10 +26,18 @@ export default class UserUsecasesImpl implements UserUseCases {
     }
 
     async update_user(id: string, entity: Partial<UserEntity>): Promise<void> {
-        let entity_to_update = await this.user_repository.orm.update(id, entity);
+        try {
+            let entity_to_update = await this.user_repository.orm.update(id, entity);
 
-        if (entity_to_update.affected) {
-            //
+            if (entity_to_update.affected && entity.email) {
+                await this.event_bus.publish<UserUpdatedPayload>('user.updated', {email: entity.email});
+            }
+
+            if (entity_to_update.affected && entity.phone) {
+                await this.event_bus.publish<UserUpdatedPayload>('user.updated', {phone: entity.phone});
+            }
+        } catch (e) {
+            throw e
         }
     }
 
--- src/shared/domain/models/event-models/user-events.ts
+++ src/shared/domain/models/event-models/user-events.ts
@@ -4,3 +4,7 @@ export interface UserCreatedPayload extends EventPayload {
     email: string,
 }
 
+export interface UserUpdatedPayload extends EventPayload {
+    email?: string,
+    phone?: string
+}
