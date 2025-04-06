ab2edd0ed10a39daf796e8f9cb32bded791479ae
c1r5dev <contato.ericjoseph@gmail.com>
2025-04-06 02:16:37 -0300

    feat: enhance application structure with new notification strategies, email verification, and environment variable management

--- src/shared/domain/repositories/transaction-repository.ts
+++
@@ -1,4 +0,0 @@
-import { Repository } from "typeorm";
-import TransactionEntity from "../../../modules/transaction/domain/models/transaction-model";
-
-export default interface TransactionRepository { }
--- src/shared/domain/values/phone.ts
+++ src/shared/domain/values/phone.ts
@@ -8,7 +8,7 @@ export class UserPhone implements ValueObject {
   }
 
   static from(phone: string): UserPhone {
-    if (/^(?:\+)[0-9]{2}\s?(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/.test(phone)) {
+    if (/^\+[0-9]{2}\s?\([0-9]{2}\)\s?[0-9]{4,5}-[0-9]{4}$/.test(phone)) {
       throw new Error("Invalid phone number format.");
     }
     return new UserPhone(phone);
